12bae08ddd5ce531ecd9ba3d48e86defb65316be
Felipe Gusmao <fgunix@gmail.com>
2020-03-15 17:38:57 -0300

    Updated API

--- ain-api/main.py
+++ ain-api/main.py
@@ -1,10 +1,30 @@
 from enum import Enum
+from typing import List, Optional
 
 from fastapi import FastAPI
+from pydantic.main import BaseModel
 from starlette.responses import RedirectResponse
+from .search_db import get_results
+from .search_es import get_es_results
+
+app = FastAPI(
+    title="AIN API",
+    description="This is an AIN data search API",
+    version="0.1.0",
+)
 
 
-app = FastAPI()
+class SearchOut(BaseModel):
+    patentes: List[Optional[dict]]
+    programas: List[Optional[dict]]
+    cultivar: List[Optional[dict]]
+    equipamentos: List[Optional[dict]]
+    laboratorios: List[Optional[dict]]
+    marcas: List[Optional[dict]]
+    empresas: List[Optional[dict]]
+    empresas_juniores: List[Optional[dict]]
+    documentos: List[Optional[dict]]
+
 
 class SearchType(str, Enum):
     all = 'all'
@@ -20,20 +40,19 @@ class SearchType(str, Enum):
     map = 'map'
 
 
-@app.get("/")
+@app.get("/", tags=['documentation'])
 async def index():
     response = RedirectResponse(url="/docs")
     return response
 
-@app.get("/search/{search_type}")
-def search(search_type: SearchType, q: str = None):
-    if search_type == SearchType.alexnet:
-        asdf = 'asdf'
-    if q:
+
+@app.get("/search/{search_type}", response_model=SearchOut, tags=['search'])
+def search(search_type: SearchType = 'all', q: str = None):
+    if search_type == SearchType.all:
+        if q:
+            results = get_results(q)
+            results['lattesuser'] = get_es_results(q)
+            return results
         return {
-            "type": f"{search_type}",
-            "term": f"{q}",
+            "error": "Please specify a search term",
         }
-    return {
-        "type": f"{search_type}",
-    }
