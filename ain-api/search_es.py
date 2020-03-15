ffd73742461ed7e9ebb626cc68f36ff279abe0c1
Felipe Gusmao <fgunix@gmail.com>
2020-03-15 17:37:59 -0300

    Updated es search service

--- ain-api/search_es.py
+++ ain-api/search_es.py
@@ -1,83 +1,19 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
 from elasticsearch import Elasticsearch
+from .config import ES_CLIENT_URL
 
 # es_client = Elasticsearch(["elastic:changeme@localhost:9200"])
-es_client = Elasticsearch(["200.136.215.138:9200"])
+es_client = Elasticsearch([ES_CLIENT_URL])
 
-user_request = "vidro"
-query_body = {
-    "_source": ["nome*", "area*", "campus", "centro", "departamento", "email", "email2", "foto", "idcnpq",
-                "informacoes_mostradas_nosite", "nome", "publico", "sigla_*", "tipo_*", "uid"],
-    "query": {
-        "multi_match": {
-            'query': user_request,
-            'fields': ["lattes2", "lattes", "area*", "nome", "departamento", "campus", "sigla_*"]
-        },
-    }
-}
-
-# call the client's search() method, and have it return results
-result = es_client.search(index="lattesuser", body=query_body, size=10000)
-
-# see how many "hits" it returned using the len() function
-print("total hits:", len(result["hits"]["hits"]))
-print("query hits:", result["hits"]["hits"])
-
-all_hits = result['hits']['hits']
-
-# iterate the nested dictionaries inside the ["hits"]["hits"] list
-def seeHits(hits):
-    for num, doc in enumerate(hits):
-        print("DOC ID:", doc["_id"], "--->", doc, type(doc), "\n")
-
-        # Use 'iteritems()` instead of 'items()' if using Python 2
-        for key, value in doc.items():
-            print(key, "-->", value)
-
-        # print a few spaces between each doc for readability
-        print("\n\n")
-
-query_body = {
-    "query": {
-        "bool": {
-            "must": {
-                "match": {
-                    "some_field": user_request
-                }
-            }
+def get_es_results(q):
+    query_body = {
+        "_source": ["nome*", "area*", "campus", "centro", "departamento", "email", "email2", "foto", "idcnpq",
+                    "informacoes_mostradas_nosite", "nome", "publico", "sigla_*", "tipo_*", "uid"],
+        "query": {
+            "multi_match": {
+                'query': q,
+                'fields': ["lattes2", "lattes", "area*", "nome", "departamento", "campus", "sigla_*"]
+            },
         }
     }
-}
-
-query_body1 = {
-    "query": {
-        "match": {
-            "some_field": "search_for_this"
-        }
-    }
-}
-
-query_body2 = {
-    "query": {
-        "match_all": {}
-    }
-}
-
-query_body3 = {
-    "query": {
-        "match": {
-            "_all": "search body"
-        }
-    }
-}
-
-
-# from elasticsearch_dsl import MultiSearch, Search
-#
-# ms = MultiSearch(index='post')
-#
-# ms = ms.add(Search().filter('term', tags='title'))
-# ms = ms.add(Search().filter('term', tags='body'))
-#
-# responses = ms.execute()
+    result = es_client.search(index="lattesuser", body=query_body, size=10000)
+    return [ item['_source'] for item in result['hits']['hits'] ]
