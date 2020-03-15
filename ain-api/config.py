85161b9775508c63a1e6048300711afe71d7ebfa
Felipe Gusmao <fgunix@gmail.com>
2020-03-15 17:38:26 -0300

    Added config file

---
+++ ain-api/config.py
@@ -0,0 +1,13 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
+ES_CLIENT_URL = os.getenv("ES_CLIENT_URL")
+DB_HOST = os.getenv("DB_HOST")
+DB_USER = os.getenv("DB_USER")
+DB_PASS = os.getenv("DB_PASS")
+DB_NAME = os.getenv("DB_NAME")
+
+
+# print(ES_CLIENT_URL)
+# print(DB_HOST, DB_USER, DB_PASS, DB_NAME)
