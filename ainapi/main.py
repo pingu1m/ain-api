27273e77472e4670bb4eb5832033bb91debed956
Felipe Gusmao <fgunix@gmail.com>
2020-03-16 21:12:35 -0300

    Updated main api file

--- ainapi/main.py
+++ ainapi/main.py
@@ -1,13 +1,20 @@
 from enum import Enum
 from typing import List, Optional
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic.main import BaseModel
 from starlette.responses import RedirectResponse
-from services import search_db, search_es
+from search_es import get_es_results
+from search_db import get_results
+from database import get_db
+from sqlalchemy.orm import Session
+
 
 app = FastAPI(
     title="AIN API",
+    docs_url="/api/docs",
+    redoc_url="/api/redoc",
+    openapi_url="/api/openapi.json",
     description="This is an AIN data search API",
     version="0.1.0",
 )
@@ -23,6 +30,7 @@ class SearchOut(BaseModel):
     empresas: List[Optional[dict]]
     empresas_juniores: List[Optional[dict]]
     documentos: List[Optional[dict]]
+    lattesuser: List[Optional[dict]]
 
 
 class SearchType(str, Enum):
@@ -39,14 +47,14 @@ class SearchType(str, Enum):
     map = 'map'
 
 
-@app.get("/", tags=['documentation'])
+@app.get("/api/", tags=['documentation'])
 async def index():
-    response = RedirectResponse(url="/docs")
+    response = RedirectResponse(url="/api/docs")
     return response
 
 
-@app.get("/search/{search_type}", response_model=SearchOut, tags=['search'], summary="Search endpoint")
-def search(search_type: SearchType = 'all', q: str = None):
+@app.get("/api/search/{search_type}", response_model=SearchOut, tags=['search'], summary="Search endpoint")
+def search(search_type: SearchType = 'all', q: str = None, db: Session = Depends(get_db) ):
     """
     Search for a term in the AIN data repository
 
@@ -55,8 +63,9 @@ def search(search_type: SearchType = 'all', q: str = None):
     """
     if search_type == SearchType.all:
         if q:
-            results = search_db.get_results(q)
-            results['lattesuser'] = search_es.get_es_results(q)
+            print(db)
+            results = get_results(db, q)
+            results['lattesuser'] = get_es_results(q)
             return results
         return {
             "error": "Please specify a search term",
